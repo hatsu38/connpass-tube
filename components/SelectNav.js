0b6b8383b98ebec142bfc355f7c339a09717556c
hatsu38 <hajiwata0308@gmail.com>
2020-07-05 20:19:42 +0900

    Add Selectable Search Range Nav

---
+++ components/SelectNav.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+import { Nav } from 'react-bootstrap';
+
+export default class SelectNav extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Nav fill variant="tabs" defaultActiveKey={this.props.range}>
+        <Nav.Item>
+          <Nav.Link eventKey="/">
+            最近
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="monhly">
+            月
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="all">
+            すべて
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    )
+  }
+}
+
+SelectNav.propTypes = {
+  range: PropTypes.string.isRequired,
+}
--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,71 @@ import React, { Component } from 'react'
 import Layout from '../components/Layout'
 import EventList from '../components/EventList'
 
+import { Nav, Card } from 'react-bootstrap';
+
 import axios from 'axios'
 
 const REQUEST_API_BASE_URL = "https://connpass-tube-api.herokuapp.com/api/v1/events"
-
+const DEFALT_RANGE = "recent"
 export default class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [],
+      range: 'recent',
+      totalEventsCount: 0
     }
     this.fetchEvents = this.fetchEvents.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
   }
 
   UNSAFE_componentWillMount() {
     this.fetchEvents()
   }
 
-  async fetchEvents() {
-    const apiResponse = await axios.get(REQUEST_API_BASE_URL)
+  handleSelect = (eventKey) =>  {
+    console.log("eventKey", eventKey);
+    this.setState({range: eventKey})
+    this.fetchEvents(eventKey)
+  }
+
+  async fetchEvents(range = DEFALT_RANGE) {
+    console.log(`${REQUEST_API_BASE_URL}?range=${range}`)
+    const apiResponse = await axios.get(`${REQUEST_API_BASE_URL}?range=${range}`)
     if(!apiResponse || !apiResponse.data) { return null }
 
-    const events = apiResponse.data.events
-    this.setState({events: events})
+
+    const eventData = apiResponse.data
+    this.setState({events: eventData.events})
+    this.setState({totalEventsCount: eventData.total_count})
   };
 
   render() {
     return (
       <>
         <Layout>
+          <Card className="text-center">
+            <Card.Body>
+              <Card.Title>{this.state.totalEventsCount}件のイベント</Card.Title>
+            </Card.Body>
+          </Card>
+          <Nav fill variant="tabs" defaultActiveKey={this.state.range} onSelect={this.handleSelect}>
+            <Nav.Item>
+              <Nav.Link eventKey="recent">
+                最近
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="monthly">
+                月
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="all">
+                すべて
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
           <EventList events={this.state.events} />
         </Layout>
       </>
